a519bc6d142577dc623059c2d303125ca419cf87
Robin C. Hoffpauir <91177807+RobinCHoffpauir@users.noreply.github.com>
2021-12-03 15:51:52 -0600

    files commit 1.0.2
    commit more files

---
+++ cryptocurrencychart.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  1 04:51:22 2021
+
+@author: robin
+"""
+cryptocurrencychart api
+[default]
+KEY = {{f114a0db98d9390397b1a0c9aa3523e5}}
+SECRET = {{2421c289fd43b52fdfa15e04c0b94af9}}
+BASE_CURRENCY = USD
